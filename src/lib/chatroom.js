cb93e090413562c2a394a3e98cfc9513a0103fe8
Lizabeth Petersen <lizpetersen928@gmail.com>
2018-06-18 16:53:33 -0700

    reformats code in server dot js

--- src/lib/chatroom.js
+++ src/lib/chatroom.js
@@ -3,3 +3,10 @@
 const EventEmitter = require('events');
 const logger = require('logger');
 const User = require('../models/users');
+
+
+const dispatchAction = (user, buffer) => {
+    const entry = parseData(buffer);
+    console.log(entry, 'This is the ENTRY');
+    if (entry) event.emit(entry.command, entry, user);
+};
--- src/lib/server.js
+++ src/lib/server.js
@@ -1,34 +1,25 @@
 'use strict';
 
-const EventEmitter = require('events');
-const net = require('net');
-const logger = require('logger');
-const User = require('../models/users');
-
 const PORT = process.env.PORT || 3000;
+
+const logger = ('logger');
 
 const server = net.createServer();
 const event = new EventEmitter();
-const socketPool = {};
-
-const dispatchAction = (user, buffer) => {
-    const entry = parseData(buffer);
-    console.log(entry, 'This is the ENTRY');
-    if (entry) event.emit(entry.command, entry, user);
-};
-
-server.listen(PORT, () => {
-    logger.log(logger.INFO, `Server is listening on Port: ${PORT}`);
-});
+const clientPool = {};
 
 server.on('connection', (socket) => {
     const user = new User (socket);
     socket.write(`Welcome to my chatroom ${user.nickname}!\n`);
-    socketPool[user._id] = user;
+    clientPool[user._id] = user;
     logger.log(logger.INFO, `A new user named ${user_nickname} has entered the chatroom`);
 
     socket.on('data', (buffer) => {
         console.log(buffer);
         dispatchAction(user, buffer);
     });
+});
+
+server.listen(PORT, () => {
+    logger.log(logger.INFO, `Server is listening on Port: ${PORT}`);
 });
